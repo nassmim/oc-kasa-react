516f6f3d958043981300417df183e4ae68bfd968
nassmim <nassmim972@gmail.com>
2023-04-26 22:19:38 +0000

    home page finished

---
+++ src/api/flats.js
@@ -0,0 +1,45 @@
+// --------------- API associée aux appartements ---------------
+
+const API_URL = "./data/flats.json"
+
+/* Récupère l'ensemble des logements
+    Paramètres
+        - Aucun
+    Retourne
+        - les logements ou une erreur du serveur
+*/
+async function getFlats() {
+  // Requête l'API
+  const flats = await fetchFlatsFromApi(API_URL)
+  return flats
+}
+
+/* Récupère un logement 
+    Paramètres
+        - id: (number) correspondant à l'id du logement
+    Retourne
+        - le logement ou une erreur du serveur
+*/
+async function getFlat(id) {
+  // Requête l'API
+  const flats = await fetchFlatsFromApi(API_URL)
+  const flat = flats.find((flat) => flat.id === id)
+  return flat
+}
+
+/* Fetch l'url pour récupérer l'ensemble des logements
+    Paramètres
+        - Aucun
+    Retourne
+        - la réponse retournée par le serveur
+*/
+async function fetchFlatsFromApi() {
+  const response = await fetch(API_URL)
+  if (response.ok) {
+    return await response.json()
+  } else {
+    throw new Error(`Mauvaise réponse du serveur - ${response}`)
+  }
+}
+
+export { getFlats, getFlat }
--- src/pages/ErrorNotFound.jsx
+++ src/pages/ErrorNotFound.jsx
@@ -1,17 +1,26 @@
 import { useRouteError } from "react-router-dom"
 
+import Header from "../components/Header.jsx"
+import Footer from "../components/Footer.jsx"
+
+import "../css/reset.css"
+import "../css/index.css"
 import "../css/errorNotFound.css"
 
 export default function ErrorNotFound() {
   const error = useRouteError()
 
   return (
-    <div className="error">
-      <h1 className="error__title">Ooops</h1>
-      <p className="error__subtitle">
-        <i>Désolé il y a eu un souci : </i>
-      </p>
-      <p className="error__message">{error.statusText || error.message}</p>
-    </div>
+    <>
+      <Header />
+      <div className="error">
+        <h1 className="error__title">Ooops</h1>
+        <p className="error__subtitle">
+          <i>Désolé il y a eu un souci : </i>
+        </p>
+        <p className="error__message">{error.statusText || error.message}</p>
+      </div>
+      <Footer />
+    </>
   )
 }
