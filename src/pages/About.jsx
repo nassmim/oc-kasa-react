03347f77bb13e4ab2972295832d77866406c68ed
nassmim <nassmim972@gmail.com>
2023-04-29 14:50:03 +0000

    afficher tous les collapsibles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,19 +2,26 @@ import Banner from "../components/Banner.jsx"
 import bannerImg from "../assets/backgrounds/about_banner.png"
 import aboutCSS from "../css/about.module.css"
 import Collapsible from "../components/Collapsible.jsx"
+import aboutSections from "../assets/data/about_sections.json"
 
 export default function About() {
   return (
     <>
       <Banner bannerClass={aboutCSS.bannerDisplay} bannerImg={bannerImg} />
+
       <main className={aboutCSS.main}>
-        <section className={aboutCSS.section}>
-          <Collapsible
-            titleClass={aboutCSS.collapsileTitle}
-            title="Fiabilité"
-            description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-          />
-        </section>
+        {aboutSections.map((section, index) => (
+          <section
+            className={aboutCSS.section}
+            key={`${section.title}-${index}`}
+          >
+            <Collapsible
+              titleClass={aboutCSS.collapsileTitle}
+              title={section.title}
+              description={section.description}
+            />
+          </section>
+        ))}
       </main>
     </>
   )
