98892ba9179f66932f76f5898e594e69c0b909f7
nassmim <nassmim972@gmail.com>
2023-04-30 03:04:10 +0100

    integration et logique du carousel

---
+++ src/components/SlideShow.jsx
@@ -0,0 +1,41 @@
+import { useRef, useState } from "react"
+import slideShowCSS from "../css/slideshow.module.css"
+import arrowLeft from "../assets/left_arrow.png"
+import arrowRight from "../assets/right_arrow.png"
+
+export default function SlideShow({ pictures }) {
+  const [slideIndex, setSlideIndex] = useState(0)
+
+  const goToPreviousSlide = () => {
+    const isFirstSlide = slideIndex === 0
+    const newSlideIndex = isFirstSlide ? pictures.length - 1 : slideIndex - 1
+    setSlideIndex(newSlideIndex)
+  }
+  const goToNextSlide = () => {
+    const isLastSlide = slideIndex === pictures.length - 1
+    const newSlideIndex = isLastSlide ? 0 : slideIndex + 1
+    setSlideIndex(newSlideIndex)
+  }
+  return (
+    <>
+      <div className={slideShowCSS.slideshow}>
+        <img
+          src={arrowLeft}
+          alt="Slide Précédente"
+          className={slideShowCSS.arrow + " " + slideShowCSS.left}
+          onClick={goToPreviousSlide}
+        />
+        <div
+          className={slideShowCSS.slide}
+          style={{ backgroundImage: `url(${pictures[slideIndex]})` }}
+        ></div>
+        <img
+          src={arrowRight}
+          alt="Slide Suivante"
+          className={slideShowCSS.arrow + " " + slideShowCSS.right}
+          onClick={goToNextSlide}
+        />
+      </div>
+    </>
+  )
+}
--- src/pages/Flat.jsx
+++ src/pages/Flat.jsx
@@ -1,5 +1,16 @@
+import SlideShow from "../components/SlideShow.jsx"
+
 export default function Flat() {
-  ;<>
-    <div>Test</div>
-  </>
+  const pictures = [
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg",
+  ]
+  return (
+    <>
+      <SlideShow pictures={pictures} />
+    </>
+  )
 }
